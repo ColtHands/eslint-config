9e2daaa4dd1fe66ebb2d48042386573eeca4e455
ColtHands <f1ks3r@yandex.ru>
2022-06-02 12:02:14 +0300

    initial2

--- src/index.js
+++ src/index.js
@@ -1,4 +1,63 @@
-(async() => {
-    const hello = await new Promise(resolve => setTimeout(() => resolve('hello world'), 1000))
-    console.log(hello)
-})()
+globalsmodule.exports = {
+    parser: "@babel/eslint-parser",
+    parserOptions: {
+        "ecmaVersion": 2020
+    },
+    globals: {
+        window: true,
+        document: true
+    },
+    env: {
+        "browser": true,
+        "es6": true,
+        "node": true,
+        "commonjs": true,
+        "jquery": true
+    },
+    rules: {
+        "no-prototype-builtins": ["off", "never"],
+        "semi": ["warn", "never"],
+        "quotes": ["warn", "single", { "avoidEscape": true }],
+        "indent": ["warn", 4, { "SwitchCase": 1 }],
+        "prefer-const": ["warn", { "destructuring": "any" }],
+        "brace-style": ["warn", "1tbs", { "allowSingleLine": true }],
+        "object-curly-spacing": ["warn", "always", { "objectsInObjects": false }],
+        "key-spacing": ["warn"],
+        "switch-colon-spacing": ["warn"],
+        "space-before-blocks": ["warn"],
+        "space-before-function-paren": ["warn", "never"],
+        "comma-spacing": ["warn", { "before": false,"after": true}],
+        "arrow-spacing": ["warn"],
+        "no-unreachable": "warn",
+        "no-useless-escape": "warn",
+        "no-unused-vars": "warn",
+        "array-bracket-spacing": ["warn", "never"],
+        "keyword-spacing": ["warn",
+            {
+                "overrides": {
+                    "if": {
+                        "after": false
+                    },
+                    "for": {
+                        "after": false
+                    },
+                    "switch": {
+                        "after": false
+                    },
+                    "while": {
+                        "after": false
+                    }
+                }
+            }
+        ],
+        "comma-dangle": ["warn",
+            {
+                "arrays": "never",
+                "objects": "never",
+                "imports": "never",
+                "exports": "never",
+                "functions": "never"
+            }
+        ]
+    }
+}
