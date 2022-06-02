bf09339ca909f26f52dd818c6d9bd900e7df56c4
ColtHands <f1ks3r@yandex.ru>
2022-06-02 12:59:42 +0300

    simple-testing-stuff

---
+++ src/test.js
@@ -0,0 +1,10 @@
+var testingQoutes = "hello"
+var testingSemicolon = 123;
+let testingPreferConst = 123;
+
+// TODO Space inside empy function call
+function testingIdentation(    ) {
+    console.log('hello world')
+}
+
+testingIdentation()
