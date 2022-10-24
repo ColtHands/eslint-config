20988b2d9665df7a922d96b56be7ab303974ae96
Aleskey.K <f1ks3r@yandex.ru>
2022-10-24 21:09:45 +0400

    Adding cypress, further configuring rules, using pnpm instead of npm

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports = {
+    root: true,
     parser: '@babel/eslint-parser',
     parserOptions: {
         ecmaVersion: 2020,
@@ -22,40 +23,30 @@ module.exports = {
         'es6': true,
         'node': true,
         'commonjs': true,
-        'jquery': true
+        'jquery': true,
+        'jest': true
     },
     extends: [
         'eslint:recommended',
-        'plugin:react/recommended'
     ],
     rules: {
-        'react/react-in-jsx-scope': 'off',
-        'no-template-curly-in-string': 'off',
-        'no-self-compare': 'warn',
-        'for-direction': 'warn',
-        'array-callback-return': ['warn', { checkForEach: true }],
-        'space-in-parens': ['warn', 'never'],
-        'no-prototype-builtins': ['off', 'never'],
-        'semi': ['warn', 'never'],
-        'quotes': ['warn', 'single', { 'avoidEscape': true }],
-        'indent': ['warn', 4, { 'SwitchCase': 1 }],
-        'prefer-const': ['warn', { 'destructuring': 'any' }],
-        'brace-style': ['warn', '1tbs', { 'allowSingleLine': true }],
-        'object-curly-spacing': ['warn', 'always', { 'objectsInObjects': false }],
-        'key-spacing': 'warn',
-        'switch-colon-spacing': 'warn',
-        'space-before-blocks': 'warn',
-        'space-before-function-paren': ['warn', {
-            'anonymous': 'always',
-            'named': 'never',
-            'asyncArrow': 'always'
-        }],
-        'comma-spacing': ['warn', { 'before': false, 'after': true }],
-        'arrow-spacing': 'warn',
-        'no-unreachable': 'warn',
-        'no-useless-escape': 'warn',
-        'no-unused-vars': 'warn',
         'array-bracket-spacing': ['warn', 'never'],
+        'array-callback-return': ['warn', { checkForEach: true }],
+        'arrow-spacing': 'warn',
+        'brace-style': ['warn', '1tbs', { 'allowSingleLine': true }],
+        'comma-dangle': ['warn',
+            {
+                'arrays': 'never',
+                'objects': 'never',
+                'imports': 'never',
+                'exports': 'never',
+                'functions': 'never'
+            }
+        ],
+        'comma-spacing': ['warn', { 'before': false, 'after': true }],
+        'for-direction': 'warn',
+        'indent': ['warn', 4, { 'SwitchCase': 1 }],
+        'key-spacing': 'warn',
         'keyword-spacing': ['warn',
             {
                 'overrides': {
@@ -74,15 +65,21 @@ module.exports = {
                 }
             }
         ],
-        'comma-dangle': ['warn',
-            {
-                'arrays': 'never',
-                'objects': 'never',
-                'imports': 'never',
-                'exports': 'never',
-                'functions': 'never'
-            }
-        ]
+        'no-prototype-builtins': ['off', 'never'],
+        'no-self-compare': 'warn',
+        'no-template-curly-in-string': 'off',
+        'no-trailing-spaces': 'error',
+        'no-unreachable': 'warn',
+        'no-unused-vars': 'warn',
+        'no-useless-escape': 'warn',
+        'object-curly-spacing': ['warn', 'always', { 'objectsInObjects': false }],
+        'prefer-const': ['warn', { 'destructuring': 'any' }],
+        'quotes': ['warn', 'single', { 'avoidEscape': true }],
+        'semi': ['warn', 'never'],
+        'space-before-blocks': 'warn',
+        'space-before-function-paren': ['warn', { 'anonymous': 'always', 'named': 'never', 'asyncArrow': 'always' }],
+        'space-in-parens': ['warn', 'never'],
+        'switch-colon-spacing': 'warn'
     },
     overrides: [
         {
@@ -91,6 +88,21 @@ module.exports = {
                 'no-undef': 'off',
                 'no-unused-vars': 'off'
             }
+        },
+        {
+            files: ['*.jsx', '*.tsx'],
+            extends: 'plugin:react/recommended',
+            rules: {
+                'react/react-in-jsx-scope': 'off'
+            }
+        },
+        {
+            files: [
+                '**/*.{cy,spec}.{js,ts,jsx,tsx}'
+            ],
+            extends: [
+                'plugin:cypress/recommended'
+            ]
         }
     ]
 }
